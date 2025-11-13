fb97fe7a9fb39dbf3b67b775d3de4b3a7504b4ce
phanngochan18 <ngochan200347@gmail.com>
2025-11-13 10:16:51 +0700

    update endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:5000/api',
-  // baseUrl: 'http://localhost:5000',
-  apiUrl: 'https://api.tinhnguyenvien.io.vn/api',
-  baseUrl: 'https://api.tinhnguyenvien.io.vn'
+  apiUrl: 'http://localhost:5000/api',
+  baseUrl: 'http://localhost:5000',
+  // apiUrl: 'https://api.tinhnguyenvien.io.vn/api',
+  // baseUrl: 'https://api.tinhnguyenvien.io.vn'
 };
